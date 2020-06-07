f347c0bb8e21869c685c9f787ba73e26f98cb325
tchleung <tom.ch.leung@gmail.com>
2020-06-07 14:38:22 -0700

    updated app script

--- tt_app.py
+++ tt_app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request
 from werkzeug.utils import secure_filename
 import os
 from helper import load_model
 from helper import wav_to_img
 from helper import make_prediction
 
-UPLOAD_FOLDER = '/upload_folder/'
+UPLOAD_FOLDER = './temp_audio/'
+ALLOWED_EXTENSIONS = {'wav'}
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 512*1024
 
 model = load_model()
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['POST', 'GET'])
 def init_recorder():
@@ -17,12 +24,16 @@ def init_recorder():
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save('temp.wav')
-      test_arr = wav_to_img('temp.wav')
-      prediction = make_prediction(model,test_arr)
-      return render_template('prediction.html', message=prediction) 
-
+    if request.method == 'POST':
+        f = request.files['file']
+        if allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            test_arr = wav_to_img(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            prediction = make_prediction(model,test_arr)
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            return render_template('prediction.html', message=prediction)
+        else:
+            return str('Format error, please go back and upload a .wav recording')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
