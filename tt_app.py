468612fa2e58e38a1b42b8d0b1546d3a82cdc7b6
tchleung <tom.ch.leung@gmail.com>
2020-06-09 21:28:13 -0700

    final clean-up, ready for heroku deployment

--- tt_app.py
+++ tt_app.py
@@ -22,20 +22,25 @@ def allowed_file(filename):
 def init_recorder():
     return render_template("index.html")
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/predict', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         if allowed_file(f.filename):
-            # filename = secure_filename(f.filename)
-            # f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            # test_arr = wav_to_img(os.path.join(app.config['UPLOAD_FOLDER'],filename))
             test_arr = wav_to_img(f)
             prediction = make_prediction(model,test_arr)
-            # os.remove(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            '''
+            Old approach saves the uploaded audio in a temp folder, removes it after prediction is made
+            Current method just loads it into memory 
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            test_arr = wav_to_img(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            '''
             return render_template('prediction.html', message=prediction)
         else:
             return str('Format error, please go back and upload a .wav recording')
 if __name__ == '__main__':
+    # Self-sign SSL to get HTTPS to work, the recorder JS requires HTTPS to work
     # app.run(host='0.0.0.0', port=8080, debug=True, ssl_context='adhoc')
     app.run(host='0.0.0.0', port=8080, debug=True)
